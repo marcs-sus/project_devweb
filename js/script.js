b9a9d83db65540975a3fd7a9e5461c50c66a0d6a
marcs-sus <lilianemarcos2018@gmail.com>
2024-10-18 13:28:04 -0300

    carousel fixes

--- js/script.js
+++ js/script.js
@@ -1,30 +1,43 @@
 // Carousel behaviour
-const track = document.querySelector('.carousel-track');
-const slides = document.querySelectorAll('.carousel-item');
-const prevButton = document.getElementById('prevBtn');
-const nextButton = document.getElementById('nextBtn');
+const track = document.querySelector(".carousel-track");
+const slides = document.querySelectorAll(".carousel-item");
+const prevButton = document.getElementById("prevBtn");
+const nextButton = document.getElementById("nextBtn");
 
 let currentIndex = 0;
+let intervalId;
 
 // Move to appointed slide
 function moveToSlide(index) {
-    const slideWidth = slides[0].offsetWidth;
-    track.style.transform = `translateX(-${slideWidth * index}px)`;
+  const slideWidth = slides[0].offsetWidth;
+  track.style.transform = `translateX(-${slideWidth * index}px)`;
+  track.style.width = `${slideWidth * slides.length}px`;
 }
 
 // Modular arithmetic for slides loop
-nextButton.onclick = function() {
-    currentIndex = (currentIndex + 1) % slides.length;
-    moveToSlide(currentIndex);
-}
+nextButton.onclick = function () {
+  currentIndex = (currentIndex + 1) % slides.length;
+  moveToSlide(currentIndex);
+  resetInterval();
+};
 
-prevButton.onclick = function() {
-    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-    moveToSlide(currentIndex);
-}
+prevButton.onclick = function () {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  moveToSlide(currentIndex);
+  resetInterval();
+};
 
 // Autoplay after 10 seconds
-setInterval(function() {
+function startInterval() {
+  intervalId = setInterval(function () {
     currentIndex = (currentIndex + 1) % slides.length;
     moveToSlide(currentIndex);
-}, 10000);
+  }, 10000);
+}
+
+function resetInterval() {
+  clearInterval(intervalId);
+  startInterval();
+}
+
+startInterval();
